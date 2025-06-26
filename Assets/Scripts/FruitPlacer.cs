4dfaf540f5a3197baa7ba17d5ee6e6427263b0d3
James Shipp <jmscshipp202@gmail.com>
2025-06-26 16:09:52 -0500

    progress on crosshair

--- Assets/Scripts/FruitPlacer.cs
+++ Assets/Scripts/FruitPlacer.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
+using UnityEngine.UIElements;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 
 public class FruitPlacer : MonoBehaviour
 {
@@ -19,16 +22,23 @@ public class FruitPlacer : MonoBehaviour
     [SerializeField]
     private GameObject peachPrefab;
 
+    [SerializeField, Tooltip("Bag of fruits for next fruit to be, more instances of one fruit will make it more likely to be chosen")]
+    private List<Fruit.Level> randomFruitBag = new List<Fruit.Level>();
+
+    // keeping track of fruit placement reqeusts to get rid of duplicates
     private List<Vector3> fruitPlacements = new List<Vector3>();
+
+    [SerializeField]
+    private Transform crossHair; // obj showing where next fruit will go
 
     // Update is called once per frame
     void Update()
     {
+        crossHair.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, crossHair.position.y, 0f);
+
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            Vector3 spawnPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            spawnPosition = new Vector3(spawnPosition.x, spawnPosition.y, 0f);
-            GameObject newFruit = Instantiate(blueBerryPrefab, spawnPosition, Quaternion.identity);
+            PlaceFruit(randomFruitBag[(int)Random.Range(0f, randomFruitBag.Count - 1)], crossHair.position);
         }
     }
 
@@ -46,33 +56,42 @@ public class FruitPlacer : MonoBehaviour
     private IEnumerator DelayedFruitPlace(Fruit.Level fruitLevel, Vector3 position)
     {
         yield return new WaitForSeconds(0.1f);
-        GameObject newFruit;
-        switch (fruitLevel)
-        {
-            case Fruit.Level.Blueberry:
-                newFruit = Instantiate(blueBerryPrefab, position, Quaternion.identity);
-                break;
-            case Fruit.Level.Raspberry:
-                newFruit = Instantiate(raspberryPrefab, position, Quaternion.identity);
-                break;
-            case Fruit.Level.Tangerine:
-                newFruit = Instantiate(tangerinePrefab, position, Quaternion.identity);
-                break;
-            case Fruit.Level.Pomegranite:
-                newFruit = Instantiate(pomegranitePrefab, position, Quaternion.identity);
-                break;
-            case Fruit.Level.Orange:
-                newFruit = Instantiate(orangePrefab, position, Quaternion.identity);
-                break;
-            case Fruit.Level.Apple:
-                newFruit = Instantiate(applePrefab, position, Quaternion.identity);
-                break;
-            case Fruit.Level.Peach:
-                newFruit = Instantiate(peachPrefab, position, Quaternion.identity);
-                break;
-        }
+
+        Instantiate(GetFruitPrefabFromLevel(fruitLevel), position, Quaternion.identity);
 
         yield return new WaitForEndOfFrame();
         fruitPlacements.Remove(position);
+    }
+
+    private GameObject GetFruitPrefabFromLevel(Fruit.Level fruitLevel)
+    {
+        GameObject newFruit = null;
+
+        switch (fruitLevel)
+        {
+            case Fruit.Level.Blueberry:
+                newFruit = blueBerryPrefab;
+                break;
+            case Fruit.Level.Raspberry:
+                newFruit = raspberryPrefab;
+                break;
+            case Fruit.Level.Tangerine:
+                newFruit = tangerinePrefab;
+                break;
+            case Fruit.Level.Pomegranite:
+                newFruit = pomegranitePrefab;
+                break;
+            case Fruit.Level.Orange:
+                newFruit = orangePrefab;
+                break;
+            case Fruit.Level.Apple:
+                newFruit = applePrefab;
+                break;
+            case Fruit.Level.Peach:
+                newFruit = peachPrefab;
+                break;
+        }
+
+        return newFruit;
     }
 }
